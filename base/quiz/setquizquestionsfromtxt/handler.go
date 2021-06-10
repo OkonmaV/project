2232e11f116e1cd178e9d34b852947544bc3713d
Magy <magicb1985@gmail.com>
2021-06-10 16:31:27 +0500

    setquizquestionsfromtxt

--- base/quiz/setquizquestionsfromtxt/handler.go
+++ base/quiz/setquizquestionsfromtxt/handler.go
@@ -1,9 +1,15 @@
 package main
 
 import (
-	"os"
+	"bytes"
+	"errors"
+	"io"
+	"mime"
+	"mime/multipart"
+	"net/url"
 	"strconv"
 	"strings"
+	"thin-peak/httpservice"
 	"thin-peak/logs/logger"
 
 	"github.com/big-larry/mgo"
@@ -13,8 +19,8 @@ import (
 )
 
 type Handler struct {
-	mgoSession *mgo.Session
-	mgoColl    *mgo.Collection
+	mgoColl *mgo.Collection
+	auth    *httpservice.InnerService
 }
 
 //quiz
@@ -32,30 +38,17 @@ type question struct {
 	Answers  map[string]string `bson:"question_answers"`
 }
 
-//
-
-func NewHandler(mgodb string, mgoAddr string, mgoColl string) (*Handler, error) {
-
-	mgoSession, err := mgo.Dial(mgoAddr)
-	if err != nil {
-		logger.Error("Mongo conn", err)
-		return nil, err
-	}
-	logger.Info("Mongo", "Connected!")
-	mgoCollection := mgoSession.DB(mgodb).C(mgoColl)
-
-	return &Handler{mgoSession: mgoSession, mgoColl: mgoCollection}, nil
-
+func NewHandler(col *mgo.Collection, auth *httpservice.InnerService) (*Handler, error) {
+	return &Handler{mgoColl: col, auth: auth}, nil
 }
 
-func (conf *Handler) Close() error {
-	conf.mgoSession.Close()
-	return nil
-}
+var eq_ch []byte = []byte("=")
+var amp_ch []byte = []byte("&")
+var field_name []byte = []byte("data")
 
 func (conf *Handler) Handle(r *suckhttp.Request, l *logger.Logger) (*suckhttp.Response, error) {
 
-	if r.GetMethod() != suckhttp.POST || !strings.Contains(r.GetHeader(suckhttp.Content_Type), "application/x-www-form-urlencoded") || len(r.Body) == 0 {
+	if r.GetMethod() != suckhttp.POST || len(r.Body) == 0 {
 		return suckhttp.NewResponse(400, "Bad request"), nil
 	}
 
@@ -64,41 +57,71 @@ func (conf *Handler) Handle(r *suckhttp.Request, l *logger.Logger) (*suckhttp.Re
 		return suckhttp.NewResponse(400, "Bad request"), nil
 	}
 
-	fileName := r.Uri.Query().Get("filename")
-	if fileName == "" {
+	var data string
+
+	if strings.Contains(r.GetHeader(suckhttp.Content_Type), "application/x-www-form-urlencoded") {
+		t := bytes.Split(r.Body, amp_ch)
+		for _, d := range t {
+			v := bytes.SplitN(d, eq_ch, 1)
+			if bytes.Equal(v[0], field_name) {
+				if unescapedString, err := url.QueryUnescape(string(v[1])); err == nil {
+					data = unescapedString
+					break
+				}
+				return suckhttp.NewResponse(400, "Bad request"), nil
+			}
+		}
+	} else if strings.Contains(r.GetHeader(suckhttp.Content_Type), "multipart/form-data") {
+		if d, err := readFromFile(r, "file"); err == nil {
+			data = string(d)
+		} else {
+			return suckhttp.NewResponse(400, "Bad request"), nil
+		}
+	} else {
 		return suckhttp.NewResponse(400, "Bad request"), nil
 	}
+
+	if len(data) == 0 {
+		return suckhttp.NewResponse(400, "Bad request"), nil
+	}
+
 	// TODO: AUTH
-	//userId := "testuserid"
-	//
-
-	fileData, err := os.ReadFile(suckutils.ConcatTwo(fileName, ".txt"))
+	// Запрашиваем у авторизатора разрешение на createquiz для текущего пользователя
+	auth_req, err := conf.auth.CreateRequestFrom(suckhttp.GET, "/createquiz?perm=1", r)
 	if err != nil {
-		return suckhttp.NewResponse(400, "Bad request"), nil
+		l.Error("Auth", err)
+		return suckhttp.NewResponse(500, "Internal Server Error"), nil
 	}
-	if len(fileData) == 0 {
-		return suckhttp.NewResponse(400, "Bad request"), nil
+	auth_resp, err := conf.auth.Send(auth_req)
+	if err != nil {
+		l.Error("Auth", err)
+		return suckhttp.NewResponse(500, "Internal Server Error"), nil
+	}
+	if code, _ := auth_resp.GetStatus(); code/100 != 2 {
+		l.Warning("Auth", "Forbidden")
+		return suckhttp.NewResponse(403, "Forbidden"), nil
 	}
 
-	lines := strings.Split(strings.TrimSpace(string(fileData)), "\n")
+	lines := strings.Split(strings.TrimSpace(data), "\n")
 
 	questions := make([]*question, 0)
 	var curquestion *question
 	for _, line := range lines {
 		if line == "" { // commit current question
-			//TODO new question
 			questions = append(questions, curquestion)
 			curquestion = nil
 		} else if curquestion == nil { // new question
 			space_position := strings.Index(line, " ")
 			if space_position == -1 {
-				//TODO error
+				l.Error("FormatError", errors.New(line))
+				return suckhttp.NewResponse(400, "Bad request"), nil
 			}
-			t, err := strconv.Atoi(line[:space_position])
-			if err != nil {
-				// TODO error
+			if t, err := strconv.Atoi(line[:space_position]); err == nil {
+				curquestion = &question{Type: t, Text: strings.TrimSpace(line[space_position+1:]), Answers: make(map[string]string)}
+			} else {
+				l.Error("ParseError", errors.New(line))
+				return suckhttp.NewResponse(400, "Bad request"), nil
 			}
-			curquestion = &question{Type: t, Text: strings.TrimSpace(line[space_position+1:]), Answers: make(map[string]string)}
 		} else { // answer
 			curquestion.Answers[bson.NewObjectId().Hex()] = strings.TrimSpace(line)
 		}
@@ -107,4 +130,34 @@ func (conf *Handler) Handle(r *suckhttp.Request, l *logger.Logger) (*suckhttp.Re
 	//TODO save
 
 	return suckhttp.NewResponse(200, "OK"), nil
+}
+
+func readFromFile(req *suckhttp.Request, name string) ([]byte, error) {
+	_, params, err := mime.ParseMediaType(req.GetHeader("content-type"))
+	if err != nil {
+		return nil, err
+	}
+	reader := bytes.NewReader(req.Body)
+	mr := multipart.NewReader(reader, params["boundary"])
+	var filedata []byte
+	for {
+		p, err := mr.NextPart()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return nil, err
+		}
+		if p.FormName() != name {
+			continue
+		}
+		filedata, err = io.ReadAll(p)
+		if err != nil {
+			return nil, err
+		}
+	}
+	if filedata == nil {
+		return nil, errors.New(suckutils.ConcatThree("Field ", name, " not found"))
+	}
+	return filedata, nil
 }
--- base/quiz/setquizquestionsfromtxt/main.go
+++ base/quiz/setquizquestionsfromtxt/main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"context"
 	"thin-peak/httpservice"
+
+	"github.com/big-larry/mgo"
+	"github.com/thin-peak/logger"
 )
 
 type config struct {
@@ -11,6 +14,7 @@ type config struct {
 	MgoDB        string
 	MgoAddr      string
 	MgoColl      string
+	mgoSession   *mgo.Session
 }
 
 var thisServiceName httpservice.ServiceName = "quiz.setquizquestionsfromtxt"
@@ -18,14 +22,28 @@ var thisServiceName httpservice.ServiceName = "quiz.setquizquestionsfromtxt"
 func (c *config) GetListenAddress() string {
 	return c.Listen
 }
+
 func (c *config) GetConfiguratorAddress() string {
 	return c.Configurator
 }
-func (c *config) CreateHandler(ctx context.Context, connectors map[httpservice.ServiceName]*httpservice.InnerService) (httpservice.HttpService, error) {
 
-	return NewHandler(c.MgoDB, c.MgoAddr, c.MgoColl)
+func (c *config) CreateHandler(ctx context.Context, connectors map[httpservice.ServiceName]*httpservice.InnerService) (httpservice.HttpService, error) {
+	mgoSession, err := mgo.Dial(c.MgoAddr)
+	if err != nil {
+		logger.Error("Mongo", err)
+		return nil, err
+	}
+	c.mgoSession = mgoSession
+	logger.Info("Mongo", "Connected!")
+	mgoCollection := mgoSession.DB(c.MgoDB).C(c.MgoColl)
+	return NewHandler(mgoCollection, connectors[httpservice.ServiceName("auth.get")])
+}
+
+func (conf *config) Close() error {
+	conf.mgoSession.Close()
+	return nil
 }
 
 func main() {
-	httpservice.InitNewService(thisServiceName, false, 5, &config{})
+	httpservice.InitNewService(thisServiceName, false, 5, &config{}, httpservice.ServiceName("auth.get"))
 }
