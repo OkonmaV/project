be40220759391bc069d226b9b74e3e13ceb8acd0
Magy <magicb1985@gmail.com>
2021-06-15 20:44:02 +0500

    protocol main.go

--- diplom/protocol/main.go
+++ diplom/protocol/main.go
@@ -2,24 +2,19 @@ package main
 
 import (
 	"context"
-	"thin-peak/httpservice"
-	"thin-peak/logs/logger"
 
-	"github.com/big-larry/mgo"
+	"thin-peak/httpservice"
 )
 
 type config struct {
 	Configurator string
 	Listen       string
-	MgoDB        string
-	MgoAddr      string
-	MgoColl      string
-	mgoSession   *mgo.Session
 }
 
-const thisServiceName httpservice.ServiceName = "quiz.createquiz"
+const thisServiceName httpservice.ServiceName = "protocol.create"
+const getuserdataServiceName httpservice.ServiceName = "identity.getuserdata"
+const getquizresultsServiceName httpservice.ServiceName = "quiz.getquizresults"
 const tokenDecoderServiceName httpservice.ServiceName = "identity.tokendecoder"
-const authSetServiceName httpservice.ServiceName = "auth.set"
 
 func (c *config) GetListenAddress() string {
 	return c.Listen
@@ -28,22 +23,10 @@ func (c *config) GetConfiguratorAddress() string {
 	return c.Configurator
 }
 func (c *config) CreateHandler(ctx context.Context, connectors map[httpservice.ServiceName]*httpservice.InnerService) (httpservice.HttpService, error) {
-	mgoSession, err := mgo.Dial(c.MgoAddr)
-	if err != nil {
-		logger.Error("Mongo", err)
-		return nil, err
-	}
-	c.mgoSession = mgoSession
-	logger.Info("Mongo", "Connected!")
-	mgoCollection := mgoSession.DB(c.MgoDB).C(c.MgoColl)
-	return NewHandler(mgoCollection, connectors[authSetServiceName], connectors[tokenDecoderServiceName])
-}
 
-func (c *config) Close() error {
-	c.mgoSession.Close()
-	return nil
+	return NewHandler(connectors[getuserdataServiceName], connectors[getquizresultsServiceName], connectors[tokenDecoderServiceName])
 }
 
 func main() {
-	httpservice.InitNewService(thisServiceName, false, 5, &config{}, tokenDecoderServiceName, authSetServiceName)
+	httpservice.InitNewService(thisServiceName, false, 5, &config{}, getuserdataServiceName, getquizresultsServiceName, tokenDecoderServiceName)
 }
